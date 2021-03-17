591130dd371ab8bcda04a793e993c2d8b33aa204
Prachachart <64405527+Prachachart@users.noreply.github.com>
2021-03-17 16:22:55 +0700

    Commit Example Code to Github

--- main.py
+++ main.py
@@ -3,6 +3,23 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+#Import Library
+import json
+import os
+
+#Global Variable
+tagKey = 'service'
+tagValue = 'production'
+
+#FileVariable
+jsonPath = r'D:\Python\CG-CSPM-TagRuleset\JSON'
+OriginalFileName = 'ExampleRule.json'
+ExportFileName = 'ExportRule.json'
+
+with open(jsonPath + '\\' + OriginalFileName) as f:
+  data = json.load(f)
+
+print(data)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
