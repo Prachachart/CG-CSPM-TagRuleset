f53cda60002249a597bab4befda3996260e5bc68
Prachachart <64405527+Prachachart@users.noreply.github.com>
2021-03-17 18:32:57 +0700

    Commit Example Code to Github

--- main.py
+++ main.py
@@ -10,16 +10,33 @@ import os
 #Global Variable
 tagKey = 'service'
 tagValue = 'production'
+ruleCount = 0
 
 #FileVariable
 jsonPath = r'D:\Python\CG-CSPM-TagRuleset\JSON'
 OriginalFileName = 'ExampleRule.json'
 ExportFileName = 'ExportRule.json'
 
+
+#Get List of Dictionary -> 1 JSON Object = 1 Dict
 with open(jsonPath + '\\' + OriginalFileName) as f:
   data = json.load(f)
 
+#Test Print Whole List of Dict
 print(data)
+
+#Test Print Each Dict
+print(data[0]["name"])
+print(data[1])
+
+#Test Print Value in Dict
+for i in data:
+    print(i["logic"])
+    ruleCount += 1
+
+#Test ruleCount
+print(ruleCount)
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
