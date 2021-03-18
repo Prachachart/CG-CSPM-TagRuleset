f9737a010c1a9d9db21d456343a5a9e7cf360e21
Prachachart <64405527+Prachachart@users.noreply.github.com>
2021-03-18 12:48:09 +0700

    Commit Example Code to Github

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ import json
 import os
 
 #Global Variable
-tagKey = 'service'
-tagValue = 'production'
+tagKey = 'Name'
+tagValue = 'public-1'
+tagLogic = "( tags contain [ key='" + tagKey + "' ] and tags contain [ value='" + tagValue + "' ] )"
 ruleCount = 0
+
+#Test Print tagLogic
+print(tagLogic)
 
 #FileVariable
 jsonPath = r'D:\Python\CG-CSPM-TagRuleset\JSON'
@@ -31,15 +35,29 @@ print(data[1])
 
 #Test Print Value in Dict
 for i in data:
+    print("================================================================================================")
     print(i["logic"])
+
     #split logic string by should
     logicList = i["logic"].split("should",1)
     print(logicList[0])
     print(logicList[1])
     ruleCount += 1
 
+    # If has where in logic
+    if 'where' in i["logic"]:
+        print("hasWhere!!!")
+        newLogic =  logicList[0] + "and " + tagLogic + " should" + logicList[1]
+        print(newLogic)
+    # If don't has where in logic
+    else:
+        print("noWhere!!!")
+        newLogic = logicList[0] + "where " + tagLogic + " should" + logicList[1]
+        print(newLogic)
+
 #Test ruleCount
-print(ruleCount)
+print("================================================================================================")
+print("Rule Count = " + str(ruleCount))
 
 
 def print_hi(name):
